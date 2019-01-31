3457c1365a9b12d2ad4ae5dacd55915ab7debda6
Tsung-en Hsiao <joshua841025@gmail.com>
2019-01-31 14:35:11 +0900

    Create MainServlet.java

---
+++ MainServlet.java
@@ -0,0 +1,41 @@
+package jp.co.calace;
+
+import java.io.*;
+import java.util.List;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import com.google.gson.Gson;
+
+import javax.servlet.annotation.*;
+
+@WebServlet(urlPatterns = {"/"})
+public class MainServlet extends HttpServlet {
+    private Instaget service;
+
+    @Override
+    public void init()
+    {
+        this.service = new Instaget();
+    }
+
+    @Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String url = req.getParameter("url");
+        if (url != null) {
+            List<String> imageUrls = service.getImageUrls(url);
+            res.setContentType("application/json");
+            PrintWriter out = res.getWriter();
+            Gson gson = new Gson();
+            out.print(gson.toJson(imageUrls));
+            out.close();
+        }
+    }
+    
+    @Override
+    public void destroy()
+    {
+        service.close();
+    }
+}
